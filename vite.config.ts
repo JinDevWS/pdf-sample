ab2e8e6eeb071ef611e9b9a4d53101848de9f8a6
JinDevWS <jindev2024@gmail.com>
2025-06-18 19:01:04 +0900

    chore: ci 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
-});
+  build: {
+    minify: mode === "production",
+  },
+}));
